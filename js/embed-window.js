c9b718405f2d37c65bb1a1b981e790423d9cc1d9
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2021-11-30 12:18:22 +0000

    v1.18

--- js/embed-window.js
+++ js/embed-window.js
@@ -11,8 +11,8 @@ function hideEmbed() {
 function dragElement(elmnt) {
     // Check if 'movablepart' exists, then use that to drag
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById("movablepart")) {
-        document.getElementById("movablepart").onmousedown = dragMouseDown;
+    if (document.getElementById("movablewindow")) {
+        document.getElementById("movablewindow").onmousedown = dragMouseDown;
     } else {
         elmnt.onmousedown = dragMouseDown;
     }
