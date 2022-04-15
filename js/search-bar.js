b0b0623c495e540d81917cdb974b88a0115c7866
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-16 01:27:35 +0200

    Removes search engine from q.value, 1 more setting
    See changes in the readme

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,10 +1,12 @@
 const f  = document.getElementById("form");
 const q  = document.getElementById("query");
 const se = document.getElementById("searchEngineIcon");
-const search_engine = "https://www.duckduckgo.com/?q=";
-var use_search_engine_icons = true;     // TODO: Get this from the settings (localstorage)
-var actual_search = "";
+const default_search_engine = "https://www.duckduckgo.com/?q=";
+var user_search_engine = "";
 
+// TODO: Get these from the settings (localstorage)
+var use_search_engine_icons = true;
+var delete_whole_se = true;
 
 const search_engines = {
     d:  "https://www.duckduckgo.com/?q=",
@@ -14,61 +16,77 @@ const search_engines = {
     yt: "https://www.youtube.com/results?search_query="
 };
 
-function hasSearchEngine(text) {
-    return (text.split(" ")[0] in search_engines && text.includes(" "));
-}
-
 function checkSearchEngine() {
-    if (use_search_engine_icons) {
-        checkSearchEngine_icons()
-    } else {
-        /*
-         * TODO: Add text mode
-         */
-        checkSearchEngine_text()
+    if (hasSearchEngine(q.value) && user_search_engine === "") {
+        user_search_engine = q.value.split(" ")[0];
+        q.value = q.value.replace(user_search_engine + " ", "");
     }
-}
 
-function checkSearchEngine_icons() {
-    /*
-     * TODO: Remove search engine from value, but still compare the real value down
-     * q.value = q.value.replace(q.value.split(" ")[0] + " ", "");     // Remove search engine
-     */
-
-    if (hasSearchEngine(q.value)) {
-        const search_engine = q.value.split(" ")[0];
-        var search_engine_icon = "duckduckgo.svg";
-        var search_engine_icon_h = "25px";
-
-        if (search_engine === "g") {
-            search_engine_icon = "google.svg";
-            search_engine_icon_h = "20px";
-        } else if (search_engine === "d" || search_engine === "dd") {
-            search_engine_icon = "duckduckgo.svg";
-        } else if (search_engine === "yt") {
-            search_engine_icon = "youtube.svg";
-        } else if (search_engine === "s") {
-            search_engine_icon = "gnu.svg";
+    if (user_search_engine != "") {
+        if (use_search_engine_icons) {
+            checkSearchEngine_icons()
+        } else {
+            /*
+             * TODO: Add text mode
+             */
+            checkSearchEngine_text()
         }
-
-        se.style.display = "block";
-        se.innerHTML = `
-            <img src="images/search-engines/${search_engine_icon}" alt="Icon" height="${search_engine_icon_h}">
-        `;
     } else {
         se.style.display = "none";
         se.innerHTML = '';
     }
 }
 
+function checkSearchEngine_icons() {
+    var search_engine_icon = "duckduckgo.svg";
+    var search_engine_icon_h = "25px";
+
+    if (user_search_engine === "g") {
+        search_engine_icon = "google.svg";
+        search_engine_icon_h = "20px";
+    } else if (user_search_engine === "d" || user_search_engine === "dd") {
+        search_engine_icon = "duckduckgo.svg";
+    } else if (user_search_engine === "yt") {
+        search_engine_icon = "youtube.svg";
+    } else if (user_search_engine === "s") {
+        search_engine_icon = "gnu.svg";
+    }
+
+    se.style.display = "block";
+    se.innerHTML = `
+        <img src="images/search-engines/${search_engine_icon}" alt="Icon" height="${search_engine_icon_h}">
+    `;
+}
+
+function hasSearchEngine(text) {
+    return (text.split(" ")[0] in search_engines && text.includes(" "));
+}
+
+function checkDeleteSearchEngine() {
+    // Will check if the user pressed backspace with no value and with search engine (so he can delete it)
+    if (user_search_engine != "" && q.value === "") {
+        switch (event.key) {
+            case "Backspace":
+                if (!delete_whole_se) {
+                    // Need to put a character to delete, not space because of the function call
+                    q.value = user_search_engine + "-";
+                }
+                user_search_engine = "";
+                checkSearchEngine();
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 function submitted(event) {
     event.preventDefault();
     if (q.value.trim() !== '') {                            // String is not empty
-        if (hasSearchEngine(q.value)) {      // The first word is in search_engines
-            var search = q.value.replace(q.value.split(" ")[0] + " ", "");   // Remove keyword
-            var url = search_engines[q.value.split(" ")[0]] + search;        // Add the matching search engine
+        if (user_search_engine != "") {                     // There is a search engine, if not use default
+            var url = search_engines[user_search_engine] + q.value;        // Add the user search engine
         } else {
-            var url = search_engine + q.value;              // Use default search engine if not in search_engines
+            var url = default_search_engine + q.value;              // Use default search engine if not empty
         }
         window.location.replace(url, "_blank");
     } else {
@@ -78,4 +96,5 @@ function submitted(event) {
 }
 
 q.focus();  // Focus the search bar
+f.addEventListener("keydown", checkDeleteSearchEngine);
 f.addEventListener("submit", submitted);
