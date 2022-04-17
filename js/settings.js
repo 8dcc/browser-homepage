c3b650327375f358128bc4bb380fb0a54e30a550
r4v10l1 <29655971+r4v10l1@users.noreply.github.com>
2022-04-18 00:45:05 +0200

    Added settings and localstorage functions (~)
    The settings do not really update but I am tired

--- js/settings.js
+++ js/settings.js
@@ -1,12 +1,79 @@
-const settBack = document.getElementById('settings-background');
-var settingsw_active = false;
+const settMain = document.getElementById('settings-main-container');
+const settTab = document.getElementById('settings-tab-container');
+var settings_array;
+var settingsw_active = false;       // Toggled by the icon thing
+
+// TODO: Maybe move some of this file to update_settings.js ?
+// TODO: Settings do not change
 
 function toggleShowSettings() {
     if (settingsw_active) {
-        settBack.style.display = "none";
+        settMain.style.display = "none";
     } else {
-        settBack.style.display = "block";
+        settMain.style.display = "block";
     }
 
     settingsw_active = !settingsw_active;
 }
+
+/* --------------------------------------------------------------- */
+
+function updateLsSettings(updated_settings) {
+    localStorage.setItem('user_settings', JSON.stringify(updated_settings));
+    renderSettings(updated_settings);
+}
+
+function getLsSettings() {
+    const reference = localStorage.getItem('user_settings');
+    if (reference) {
+        settings_array = JSON.parse(reference);
+        renderSettings(settings_array);
+    }
+}
+
+function renderSettings() {
+    updateBoolSetting("sbutton-seicon", settings_array.use_se_icons);
+}
+
+function updateBoolSetting(id, setting_state) {
+    document.getElementById(id).checked = setting_state;
+}
+
+function checkEmptyLs() {
+    if (!localStorage.getItem('user_settings')) {
+        console.log("[settings] Detected no settings in localstorage. Generating defaults...")
+        var dso = new Object();
+
+        dso.use_light_theme     = false;
+        dso.use_se_icons        = true;
+
+        var default_settings = JSON.stringify(dso);
+        updateLsSettings(JSON.parse(default_settings));
+    }
+}
+
+/* --------------------------------------------------------------- */
+
+settTab.addEventListener('click', function(event) {
+    if (event.target.type === 'checkbox') {
+        toggle(event.target.parentElement.getAttribute('data-key'));
+    }
+});
+
+function toggle(changeme) {
+    Object.entries(settings_array).forEach(([key, value]) => {
+        if (key == changeme) {
+            item.setting_enabled = !item.setting_enabled;
+        }
+    })
+
+    // settings_array.forEach(function(item) {
+    //     if (item.id == id) {
+    //         item.setting_enabled = !item.setting_enabled;
+    //     }
+    // });
+    updateLsSettings(settings_array);
+}
+
+checkEmptyLs();
+getLsSettings();
