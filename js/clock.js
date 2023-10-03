a13821308b052e64cc9e840b0a49def37c3de258
8dcc <8dcc.git@gmail.com>
2023-10-03 18:38:52 +0200

    Use IBM Plex Mono for clock

--- js/clock.js
+++ js/clock.js
@@ -6,17 +6,19 @@ function currentTime() {
     hour = updateTime(hour);
     min = updateTime(min);
     sec = updateTime(sec);
-    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
-    var t = setTimeout(currentTime, 1000); /* setting timer */
+
+    document.getElementById("clock").innerText = hour + ":" + min + ":" + sec;
+
+    /* Timer */
+    var t = setTimeout(currentTime, 1000);
 }
 
-function updateTime(k) { /* appending 0 before time elements if less than 10 */
-    if (k < 10) {
+function updateTime(k) {
+    /* Add padding if less than 0 */
+    if (k < 10)
         return "0" + k;
-    }
-    else {
+    else
         return k;
-    }
 }
 
 currentTime();
