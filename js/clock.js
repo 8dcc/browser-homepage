669fe8d0f86727204b2aed16c42b88cdee85354b
8dcc <8dcc.git@gmail.com>
2024-01-26 19:18:19 +0100

    Use setInterval() in clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,19 +1,4 @@
-function currentTime() {
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-
-    document.getElementById("clock").innerText = hour + ":" + min + ":" + sec;
-
-    /* Timer */
-    var t = setTimeout(currentTime, 1000);
-}
-
-function updateTime(k) {
+function padNumber(k) {
     /* Add padding if less than 0 */
     if (k < 10)
         return "0" + k;
@@ -21,4 +6,17 @@ function updateTime(k) {
         return k;
 }
 
-currentTime();
+function updateTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = padNumber(hour);
+    min  = padNumber(min);
+    sec  = padNumber(sec);
+
+    document.getElementById("clock").innerText = hour + ":" + min + ":" + sec;
+}
+
+updateTime();
+setInterval(updateTime, 1000)
