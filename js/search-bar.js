2d67d4a39d34e7994be588815ef330182d241f23
8dcc <8dcc.git@gmail.com>
2024-03-05 18:56:25 +0100

    Completely change search bar JS to use nested objects
    Change id's for search bar form and query

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,138 +1,160 @@
-const f  = document.getElementById("form");
-const q  = document.getElementById("query");
-const se_icon = document.getElementById("search-engine-icon");
-const se_text = document.getElementById("search-engine-container");
-const default_search_engine = "https://www.startpage.com/do/dsearch?query=";
-var user_search_engine = "";
+const search_query = document.getElementById("search-query");
+const default_search_engine = "s";
+var user_search_engine = null;
 
 const search_engines = {
-    d:      "https://www.duckduckgo.com/?q=",
-    dd:     "https://html.duckduckgo.com/html/?q=",
-    g:      "https://www.google.com/search?q=",
-    sx:     "https://searx.gnous.eu/search?q=",
-    l:      "https://librex.me/search.php?q=",
-    yt:     "https://www.youtube.com/results?search_query=",
-    w:      "https://en.wikipedia.org/wiki/Special:Search?search=",
-    s:      "https://www.startpage.com/do/dsearch?query=",
-    m:      "https://man.cx/",
+    d: {
+        url: "https://www.duckduckgo.com/?q=",
+        icon: "duckduckgo.svg",
+        icon_h: "25px",
+        text: "DuckDuckGo",
+    },
+
+    dd: {
+        url: "https://html.duckduckgo.com/html/?q=",
+        icon: "duckduckgo.svg",
+        icon_h: "25px",
+        text: "DuckDuckGo HTML",
+    },
+
+    g: {
+        url: "https://www.google.com/search?q=",
+        icon: "google.svg",
+        icon_h: "20px",
+        text: "Google",
+    },
+
+    sx: {
+        url: "https://searx.gnous.eu/search?q=",
+        icon: "gnu.svg",
+        icon_h: "25px",
+        text: "SearX",
+    },
+
+    l: {
+        url: "https://librex.me/search.php?q=",
+        icon: "librex.png",
+        icon_h: "25px",
+        text: "LibreX",
+    },
+
+    yt: {
+        url: "https://www.youtube.com/results?search_query=",
+        icon: "youtube.svg",
+        icon_h: "25px",
+        text: "YouTube",
+    },
+
+    w: {
+        url: "https://en.wikipedia.org/wiki/Special:Search?search=",
+        icon: "wikipedia.png",
+        icon_h: "25px",
+        text: "Wikipedia",
+    },
+
+    s: {
+        url: "https://www.startpage.com/do/dsearch?query=",
+        icon: "startpage.png",
+        icon_h: "25px",
+        text: "Startpage",
+    },
+
+    m: {
+        url: "https://man.cx/",
+        icon: "man.png",
+        icon_h: "25px",
+        text: "Man",
+    },
 };
 
-function checkSearchEngine() {
-    if (hasSearchEngine(q.value) && user_search_engine === "") {
-        user_search_engine = q.value.split(" ")[0];
-        q.value = q.value.replace(user_search_engine + " ", "");
-    }
 
-    if (settings_global_object["use_se_icons"]) {          // Icon mode
-        if (user_search_engine != "") checkSearchEngine_icons();
-        else clearElementContents(se_icon);
-    } else {                                // Text mode
-        if (user_search_engine != "") checkSearchEngine_text();
-        else clearElementContents(se_text);
-    }
-}
+function getSearchEngineObject(text) {
+    if (!text.includes(" "))
+        return;
 
-function checkSearchEngine_icons() {
-    var search_engine_icon = "startpage.png";
-    var search_engine_icon_h = "25px";
+    // Iterate search_engines object
+    for (const [key, value] of Object.entries(search_engines))
+        // Is this the search engine that the user typed?
+        if (text.split(" ")[0] == key)
+            return value;
 
-    if (user_search_engine === "g") {
-        search_engine_icon = "google.svg";
-        search_engine_icon_h = "20px";
-    } else if (user_search_engine === "d" || user_search_engine === "dd") {
-        search_engine_icon = "duckduckgo.svg";
-    } else if (user_search_engine === "yt") {
-        search_engine_icon = "youtube.svg";
-    } else if (user_search_engine === "sx") {
-        search_engine_icon = "gnu.svg";
-    } else if (user_search_engine === "l") {
-        search_engine_icon = "librex.png";
-    } else if (user_search_engine === "w") {
-        search_engine_icon = "wikipedia.png";
-    } else if (user_search_engine === "s") {
-        search_engine_icon = "startpage.png";
-    } else if (user_search_engine === "m") {
-        search_engine_icon = "man.png";
-    }
-
-    se_icon.style.display = "flex";     // We need flex instead of block to center the image vertically
-    se_icon.innerHTML = `
-        <img src="img/search-engines/${search_engine_icon}" alt="Icon" height="${search_engine_icon_h}">
-    `;
-    q.style.paddingLeft = "10px";   // Remove some margin from actual search input
-}
-
-function checkSearchEngine_text() {
-    var search_engine_text = "DuckDuckGo";
-
-    if (user_search_engine === "g") {
-        search_engine_text = "Google";
-    } else if (user_search_engine === "yt") {
-        search_engine_text = "YouTube";
-    } else if (user_search_engine === "dd") {
-        search_engine_text = "DuckDuckGo HTML";
-    } else if (user_search_engine === "sx") {
-        search_engine_text = "SearX";
-    } else if (user_search_engine === "l") {
-        search_engine_text = "LibreX";
-    } else if (user_search_engine === "w") {
-        search_engine_text = "Wikipedia";
-    } else if (user_search_engine === "s") {
-        search_engine_text = "Startpage";
-    } else if (user_search_engine === "m") {
-        search_engine_text = "Man";
-    }
-
-    se_text.style.display = "block";
-    se_text.innerHTML = `
-        <div class="search-engine-text">${search_engine_text}</div>
-    `;
-    q.style.paddingLeft = "10px";   // Remove some margin from actual search input
-}
-
-function hasSearchEngine(text) {
-    return (text.split(" ")[0] in search_engines && text.includes(" "));
+    return null;
 }
 
 function clearElementContents(element) {
     element.style.display = "none";
-    element.innerHTML = '';
+    element.innerHTML = "";
 }
 
-function checkDeleteSearchEngine() {
+function checkSearchEngine() {
+    const se_icon = document.getElementById("search-engine-icon");
+    const se_text = document.getElementById("search-engine-container");
+
+    if (!user_search_engine)
+        user_search_engine = getSearchEngineObject(search_query.value);
+
+    if (!user_search_engine) {
+        // We haven't found a valid search engine in the loop, clear icon and text
+        clearElementContents(se_icon);
+        clearElementContents(se_text);
+
+        // Remove reduced padding that is added when using search engine
+        search_query.style.removeProperty("padding-left");
+        return;
+    }
+
+    // Remove search engine text ("d ") and add padding
+    search_query.value = search_query.value.replace(search_query.value.split(" ")[0] + " ", "");
+    search_query.style.paddingLeft = "10px";
+
+    // Check if the user wants to use icons or text
+    if (settings_global_object["use_se_icons"]) {
+        // We need flex instead of block to center the image vertically
+        se_icon.style.display = "flex";
+        se_icon.innerHTML = `<img src="img/search-engines/${user_search_engine.icon}" alt="Icon" height="${user_search_engine.icon_h}">`;
+    } else {
+        // Use block for text
+        se_text.style.display = "block";
+        se_text.innerHTML = `<div class="search-engine-text">${user_search_engine.text}</div>`;
+    }
+}
+
+// When we type in the form
+document.getElementById("search-form").addEventListener("keydown", function(e) {
     // Will check if the user pressed backspace with no value and with search engine (so he can delete it)
-    if (user_search_engine != "" && q.value === "") {
+    if (user_search_engine && search_query.value === "") {
         switch (event.key) {
             case "Backspace":
-                if (!settings_global_object["delete_whole_se"]) {
-                    // Need to put a character to delete, not space because of the function call
-                    q.value = user_search_engine + "-";
-                }
-                user_search_engine = "";
+                // Call this function again to re-check if we have a search engine. At this point we don't.
+                user_search_engine = null;
                 checkSearchEngine();
-                q.style.removeProperty("padding-left");     // Remove reduced padding that is added when using search engine
                 break;
             default:
                 break;
         }
     }
-}
+});
 
-function submitted(event) {
-    event.preventDefault();
-    if (q.value.trim() !== '') {                            // String is not empty
-        if (user_search_engine != "") {                     // There is a search engine, if not use default
-            var url = search_engines[user_search_engine] + q.value;        // Add the user search engine
+// When we submit the form
+document.getElementById("search-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // String is not empty
+    if (search_query.value.trim() !== "") {
+        if (user_search_engine) {
+            // There is a search engine
+            var url = user_search_engine.url + search_query.value;
         } else {
-            var url = default_search_engine + q.value;              // Use default search engine if not empty
+            // No user search engine, use default
+            var url = search_engines[default_search_engine].url + search_query.value;
         }
+
         window.location.replace(url, "_blank");
     } else {
-        q.value = '';
-        q.focus();
+        search_query.value = "";
+        search_query.focus();
     }
-}
+});
 
-q.focus();  // Focus the search bar
-f.addEventListener("keydown", checkDeleteSearchEngine);
-f.addEventListener("submit", submitted);
+// When we load the page, focus the search bar
+search_query.focus();
